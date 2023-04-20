24868077d66cf75ed2bb1972011d3e16cdf1baa9
seppkh <62253084+seppkh@users.noreply.github.com>
2023-04-20 13:25:27 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,49 @@
+{
+  courseUrl: "https://ois2.tlu.ee/tluois/aine/HKI5003.HK",
+  active: true,
+  docs: [
+    {
+      slug: "about",
+      name: "Aine info"
+    }
+  ],
+  additionalMaterials: [
+    {
+      slug: "lisamaterjalid",
+      name: "Aine lisamaterjalid"
+    }
+  ],
+  lessons: [
+    {
+      name: "Loeng 1",
+      slug: "loeng_01",
+      uuid: "b47bd74e-160b-41b1-80ef-d4f1335021eb",
+      components: ["Sissejuhatus", "API"],
+      additionalMaterials: [
+        {
+          slug: "lisamaterjalid",
+          name: "Loengu lisamaterjalid"
+        }
+      ]
+    }
+  ],
+  concepts: [
+    {
+      slug: "Sissejuhatus",
+      name: "Sissejuhatus",
+      uuid: "1a904ddf-1703-418e-88b6-2fd35405ecac"
+    },
+    {
+      slug: "API",
+      name: "API",
+      uuid: "fc10e172-8c46-4202-80bc-1c6cfdaeaa62"
+    }
+  ],
+  practices: [
+    {
+      slug: "praktikum_01",
+      name: "Näidis Praktikum",
+      uuid: "74dea7c7-2b51-4867-ad77-9137c91dca5b"
+    }
+  ]
+}
